4f2f80641a6a8988d7fd03227f36bc04a89f52fd
Axel Pulgar <brepundtrep@yahoo.com.ar>
2022-04-24 19:02:22 +0200

    video test

--- src/components/scene.js
+++ src/components/scene.js
@@ -5,7 +5,7 @@ import MasModel from "./LocosComponents3D/MasComponent";
 const Scene = ({ pitchValue, filterFrequency }) => {
   return (
     <group>
-      <ambientLight intensity={0.1} />
+      <ambientLight intensity={0.2} />
       <spotLight castShadow intensity={1} position={[1, 0.5, 2]} />
       <OrbitControls />
       <Model pitchValue={pitchValue} filterFrequency={filterFrequency} />
